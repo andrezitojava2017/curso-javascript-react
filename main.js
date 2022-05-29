a5f12ffe26a16b4580e876d44f297b3148ef2a7c
Jederson Andre <andre_sjx@live.com>
2022-05-29 15:47:03 -0300

    Create Object ScoreBoard

--- main.js
+++ main.js
@@ -3,14 +3,13 @@ import './src/styles/generic/reset.css';
 import './src/styles/settings/colors.css';
 import './src/styles/elements/base.css';
 import BoardGame from './src/Objects/BoardGame';
-import PlayerName from './src/components/PlayerName';
+import ScoreBoard from './src/Objects/ScoreBoard';
 
 const $root = document.querySelector('#root');
 
 $root.insertAdjacentHTML(
   'beforeend',
   `
-${PlayerName('JOGARDOR 1')}
-${PlayerName('JOGARDOR 2')}
-${BoardGame(6)}`
+  ${ScoreBoard()}
+  ${BoardGame(6)}`
 );
---
+++ src/Objects/ScoreBoard/index.js
@@ -0,0 +1,10 @@
+import PlayerName from '../../components/PlayerName';
+
+function ScoreBoard() {
+  return `
+  ${PlayerName('JOGARDOR 1')}
+  ${PlayerName('JOGARDOR 2')}
+  `;
+}
+
+export default ScoreBoard;

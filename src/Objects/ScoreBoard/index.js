b38598ccd31773b503d46785eb8d4c9c8c0d9562
Jederson Andre <andre_sjx@live.com>
2022-05-29 16:10:55 -0300

    Style css Object Score board

--- src/Objects/ScoreBoard/index.js
+++ src/Objects/ScoreBoard/index.js
@@ -1,9 +1,11 @@
 import PlayerName from '../../components/PlayerName';
-
+import './style.css';
 function ScoreBoard() {
   return `
+  <header class="score-board">
   ${PlayerName('JOGARDOR 1')}
   ${PlayerName('JOGARDOR 2')}
+  </header>
   `;
 }
 
